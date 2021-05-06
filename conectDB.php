f2e1b000ded225b6e1e256d303e4b4e4aa9ac71f
WebGreenTea <sahawut092013@gmail.com>
2021-05-06 20:13:04 +0700

    save

--- conectDB.php
+++ conectDB.php
@@ -1,8 +1,8 @@
 <?php
 
     $servername = "localhost";
-    $username = "root";
-    $password = "";
+    $username = "sahawuth";
+    $password = "12345";
     $dbname = "phonesell";
   
     $conect = mysqli_connect($servername,$username,$password,$dbname);
